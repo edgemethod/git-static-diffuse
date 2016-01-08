53c3d140322ac04b00f70cc23fbfa6b918ba6611
Alvin <alvin@domain7.com>
2016-01-08 13:31:38 -0800

    Update app.js

--- gsd-server/app.js
+++ gsd-server/app.js
@@ -14,7 +14,6 @@ var path = require('path');
 var compress = require('compression');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
-var multer = require('multer');
 var logger = require('morgan');
 var errorHandler = require('errorhandler');
 
@@ -48,7 +47,6 @@ function historyServer(reps, port_number){
   app.use(logger());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(multer());
 
   app.use(methodOverride());
   app.use(express.static(path.join(__dirname, 'public')));
